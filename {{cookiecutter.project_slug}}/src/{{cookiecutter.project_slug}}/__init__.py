6768417b9750910c2bf655b48150ee7c2f2beaf2
Dave Decker <dave.decker@gmail.com>
2017-05-29 20:41:04 -0500

    edited init and setup to avoid flake8 errors

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -7,3 +7,11 @@ from {{ cookiecutter.project_slug }}.__about__ import (
 {%- endif %}
     __uri__, __version__
 )
+
+__all__ = [
+    "__title__", "__summary__", "__uri__", "__version__", "__author__",
+    "__email__", "__copyright__",
+{%- if cookiecutter.open_source_license in license_classifiers %}
+    "__license__",
+{%- endif %}
+]
